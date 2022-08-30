42259180654116a058249af2a300745a9e6469f8
Nestor Cavallini <nestorcavallini@gmail.com>
2022-08-30 19:31:40 -0300

    Integración del menú principal al main

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ Analisis de Finanzas
 
 Esta implementación permite leer datos de una API de finanzas, guardarlos en una base de datos y graficarlos
 """
-vista.presentacion()
-vista.menu_principal()
+while True:
+    vista.presentacion()
+    opcion = vista.menu_principal()
+
+    if opcion == "1": #Actualización de Datos
+        print("Actualización de Datos")
+        print()
+        
+    elif opcion == "2": #Visualizador de Datos
+        print("Visualizador de Datos")
+        print()
+
+    elif opcion == "0": #Salida
+        break
+
+    else: #Mensaje de Error
+        print("Opción elegida incorrecta")
+        print()
